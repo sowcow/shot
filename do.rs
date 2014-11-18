220c897eff1a68ff848cc4444186b7705d699656
Alexander K <xpyro@ya.ru>
2014-11-18 15:20:48 +0500

    ppm format

--- do.rs
+++ do.rs
@@ -1,4 +1,5 @@
 
+// todo into one call
 fn main() {
 
   let input_file = "given";
@@ -12,8 +13,8 @@ fn main() {
 
 mod types {
   pub struct ImageData {
-    pub width: u8,
-    pub height: u8,
+    pub width: i32,
+    pub height: i32,
     pub data: Vec<u8>,
   }
 }
@@ -26,15 +27,14 @@ mod input {
   pub fn read(file_name: &str) -> ImageData {
     let path = Path::new(file_name);
     let mut file = File::open(&path).unwrap();
-    //
-    // todo: read header data as well
-    let width = 1;
-    let height = 1;
+
+    let width = file.read_le_i32().unwrap();
+    let height = file.read_le_i32().unwrap();
+    let _unknown = file.read_le_i32().unwrap();
     let data = file.read_to_end().unwrap();
 
     ImageData { width: width, height: height, data: data }
   }
-
 }
 
 mod data {
@@ -47,7 +47,7 @@ mod data {
       data: vec!()
     };
 
-    // each 4-th byte is just 255 for some reason
+    // each 4-th byte is just `255` for some reason
     for rgb in data.data.as_slice().chunks(4) {
       result.data.push_all([rgb[0], rgb[1], rgb[2]]);
     }
@@ -59,9 +59,16 @@ mod result {
   use std::io::File;
   use types::ImageData;
 
+  // todo: into ImageData struct impl!
+  // .ppm format
   pub fn write(image: ImageData, file_name: &str) {
     let path = Path::new(file_name);
     let mut file = File::create(&path).unwrap();
+
+    let width = image.width;
+    let height = image.height;
+    let header = format!("P6 {} {} 255\n", width, height);
+    file.write(header.as_bytes()).unwrap();
     file.write(image.data.as_slice()).unwrap();
   }
 }
